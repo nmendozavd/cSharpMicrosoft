775fe8ae203860df8e8059f4437bc2d961b58bc6
Noel Mendoza <nmendozavd@gmail.com>
2020-11-14 19:04:25 -0800

    added working with integer functions

--- Program.cs
+++ Program.cs
@@ -4,10 +4,37 @@ namespace cSharpCourse
 {
     class Program
     {
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 6;
+
+            // addition
+            int c = a + b;
+            Console.WriteLine($"Additon: {c}");
+
+            // subtration
+            c = a - b;
+            Console.WriteLine($"Subtraction: {c}");
+
+            // multiplication
+            c = a * b;
+            Console.WriteLine($"Multiplication: {c}");
+
+            // division
+            c = a / b;
+            Console.WriteLine($"Division: {c}");
+        }
+
+        static void OrderPrecedence()
+        {
+            
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("The current time is " + DateTime.Now);
+            //WorkingWithIntegers();
+            OrderPrecedence();
         }
     }
+    
 }
