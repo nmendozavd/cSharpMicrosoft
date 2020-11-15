c0d94898f7deabf7ae323d6a04d4e2c7ad10cd74
Noel Mendoza <nmendozavd@gmail.com>
2020-11-14 21:14:12 -0800

    added exploreif function

--- branchesAndLoops/Program.cs
+++ branchesAndLoops/Program.cs
@@ -4,9 +4,36 @@ namespace BranchesAndLoops
 {
     class Program
     {
+        static void ExploreIf()
+        {
+            int a = 3;
+            int b = 3;
+            if (a + b > 10)
+            {
+                Console.WriteLine("The answer is greater than 10.");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+            }
+
+            int c = 5;
+            if ((a + b + c > 10) && (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is equal to the second number");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not equal to the second");
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ExploreIf();
+            
         }
     }
 }
