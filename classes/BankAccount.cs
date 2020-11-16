307c5878aaab099745aee1b610498d87a405fc47
Noel Mendoza <nmendozavd@gmail.com>
2020-11-15 17:38:50 -0800

    completed bank account class object

--- classes/BankAccount.cs
+++ classes/BankAccount.cs
@@ -1,21 +1,85 @@
 using System;
+using System.Collections.Generic;
 
 namespace classes
 {
   public class BankAccount
   {
+    private List<Transaction> allTransactions = new List<Transaction>();
+    private static int accountNumberSeed = 1234567890;
     public string Number { get; }
     public string Owner { get; set; }
-    public decimal Balance { get; }
+    public decimal Balance 
+    { 
+      get 
+      {
+        decimal balance = 0;
+        foreach(var num in allTransactions)
+        {
+          balance += num.Amount;
+        }
+        return balance;
+      }
+    }
+    
+    public BankAccount(string name, decimal initialBalance) 
+    {
+      this.Number = accountNumberSeed.ToString();
+      accountNumberSeed++;
 
+      this.Owner = name;
+      // call method for initial deposit when creating new bank account
+      MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+    }
     public void MakeDeposit(decimal amount, DateTime date, string note) 
     {
+      // make sure deposit isn't negative, throw exception
+      if(amount <= 0)
+      {
+        throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+      }
+      // create new instance of transaction
+      var deposit = new Transaction(amount, date, note);
+      // add to transactions list
+      allTransactions.Add(deposit);
 
     }
 
     public void MakeWithdrawal(decimal amount, DateTime date, string note)
-    {
-      
+    { 
+      // check if withdrawal is negative
+      if (amount <= 0)
+      {
+        throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+      }
+      // check if withdrawal makes balance negative
+      if (Balance - amount < 0)
+      {
+        throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+      }
+      // create new instance of transaction, and add negative amount
+      var withdrawal = new Transaction(-amount, date, note);
+      allTransactions.Add(withdrawal);
     }
+    
+    public string GetAccountHistory()
+    { // string builder formats a string one line per transaction
+      var report = new System.Text.StringBuilder();
+
+      decimal balance = 0;
+      // inserts tab between characters
+      report.AppendLine("Date\t\tAmount\tBalance\tNote");
+      // loop through transactions list
+      foreach(var num in allTransactions)
+      {
+        // increment count
+        balance += num.Amount;
+        // add line to report
+        report.AppendLine($"{num.Date.ToShortDateString()}\t{num.Amount}\t{balance}\t{num.Notes}");
+      }
+      // return report, convert to string
+      return report.ToString();
+    }
+   
   }
 }
--- classes/Program.cs
+++ classes/Program.cs
@@ -5,8 +5,39 @@ namespace classes
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   // create bank account instance
+            var account = new BankAccount("Noel", 1000);
+            Console.WriteLine($"Account { account.Number } was created for { account.Owner } with ${ account.Balance } initial balance.");
+            // call withdrawal method
+            account.MakeWithdrawal(500, DateTime.Now, "Rent Payment");
+            Console.WriteLine(account.Balance);
+            // call deposit method
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance); 
+            // call account history method
+            Console.WriteLine(account.GetAccountHistory());
+
+            // Test that the initial balances must be positive.
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+    
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
