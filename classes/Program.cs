403e16a00767e46addd606a2ac02b5617f683459
Noel Mendoza <nmendozavd@gmail.com>
2020-11-16 19:03:26 -0800

    added link to tutorial

--- classes/Program.cs
+++ classes/Program.cs
@@ -17,54 +17,56 @@ namespace classes
             // // call account history method
             // Console.WriteLine(account.GetAccountHistory());
 
-            // // Test that the initial balances must be positive.
-            // try
-            // {
-            //     var invalidAccount = new BankAccount("invalid", -55);
-            // }
-            // catch (ArgumentOutOfRangeException e)
-            // {
-            //     Console.WriteLine("Exception caught creating account with negative balance");
-            //     Console.WriteLine(e.ToString());
-            // }
-    
-            // // Test for a negative balance.
-            // try
-            // {
-            //     account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
-            // }
-            // catch (InvalidOperationException e)
-            // {
-            //     Console.WriteLine("Exception caught trying to overdraw");
-            //     Console.WriteLine(e.ToString());
-            // }
+      // // Test that the initial balances must be positive.
+      // try
+      // {
+      //     var invalidAccount = new BankAccount("invalid", -55);
+      // }
+      // catch (ArgumentOutOfRangeException e)
+      // {
+      //     Console.WriteLine("Exception caught creating account with negative balance");
+      //     Console.WriteLine(e.ToString());
+      // }
 
-            // GIFT CARD ACCOUNT
-            // var giftCard = new GiftCardAccount("gift card", 100, 50);
-            // giftCard.MakeWithdrawal(20, DateTime.Now, "get expensive coffee");
-            // giftCard.MakeWithdrawal(50, DateTime.Now, "buy groceries");
-            // giftCard.PerformMonthEndTransactions();
-            // // can make additional deposits:
-            // giftCard.MakeDeposit(27.50m, DateTime.Now, "add some additional spending money");
-            // Console.WriteLine(giftCard.GetAccountHistory());
+      // // Test for a negative balance.
+      // try
+      // {
+      //     account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+      // }
+      // catch (InvalidOperationException e)
+      // {
+      //     Console.WriteLine("Exception caught trying to overdraw");
+      //     Console.WriteLine(e.ToString());
+      // }
 
-            // INTEREST EARNING ACCOUNT
-            // var savings = new InterestEarningAccount("savings account", 10000);
-            // savings.MakeDeposit(750, DateTime.Now, "save some money");
-            // savings.MakeDeposit(1250, DateTime.Now, "Add more savings");
-            // savings.MakeWithdrawal(250, DateTime.Now, "Needed to pay monthly bills");
-            // savings.PerformMonthEndTransactions();
-            // Console.WriteLine(savings.GetAccountHistory());
+      // GIFT CARD ACCOUNT
+      // var giftCard = new GiftCardAccount("gift card", 100, 50);
+      // giftCard.MakeWithdrawal(20, DateTime.Now, "get expensive coffee");
+      // giftCard.MakeWithdrawal(50, DateTime.Now, "buy groceries");
+      // giftCard.PerformMonthEndTransactions();
+      // // can make additional deposits:
+      // giftCard.MakeDeposit(27.50m, DateTime.Now, "add some additional spending money");
+      // Console.WriteLine(giftCard.GetAccountHistory());
 
-            // LINE OF CREDIT ACCOUNT
-            // var lineOfCredit = new LineOfCreditAccount("line of credit", 0, 2000);
-            // // How much is too much to borrow?
-            // lineOfCredit.MakeWithdrawal(1000m, DateTime.Now, "Take out monthly advance");
-            // lineOfCredit.MakeDeposit(50m, DateTime.Now, "Pay back small amount");
-            // lineOfCredit.MakeWithdrawal(5000m, DateTime.Now, "Emergency funds for repairs");
-            // lineOfCredit.MakeDeposit(150m, DateTime.Now, "Partial restoration on repairs");
-            // lineOfCredit.PerformMonthEndTransactions();
-            // Console.WriteLine(lineOfCredit.GetAccountHistory());
-        }
+      // INTEREST EARNING ACCOUNT
+      // var savings = new InterestEarningAccount("savings account", 10000);
+      // savings.MakeDeposit(750, DateTime.Now, "save some money");
+      // savings.MakeDeposit(1250, DateTime.Now, "Add more savings");
+      // savings.MakeWithdrawal(250, DateTime.Now, "Needed to pay monthly bills");
+      // savings.PerformMonthEndTransactions();
+      // Console.WriteLine(savings.GetAccountHistory());
+
+      // LINE OF CREDIT ACCOUNT
+      // var lineOfCredit = new LineOfCreditAccount("line of credit", 0, 2000);
+      // // How much is too much to borrow?
+      // lineOfCredit.MakeWithdrawal(1000m, DateTime.Now, "Take out monthly advance");
+      // lineOfCredit.MakeDeposit(50m, DateTime.Now, "Pay back small amount");
+      // lineOfCredit.MakeWithdrawal(5000m, DateTime.Now, "Emergency funds for repairs");
+      // lineOfCredit.MakeDeposit(150m, DateTime.Now, "Partial restoration on repairs");
+      // lineOfCredit.PerformMonthEndTransactions();
+      // Console.WriteLine(lineOfCredit.GetAccountHistory());
+
+      // https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/intro-to-csharp/object-oriented-programming
+    }
     }
 }
