b1056e03cb87a46718e69f4ae9697ad395340502
Noel Mendoza <nmendozavd@gmail.com>
2020-11-26 16:04:11 -0800

    completed splitting and searching string sections

--- exercises1/Program.cs
+++ exercises1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace exercises1
 {
@@ -65,8 +67,93 @@ namespace exercises1
             double b = 12.3;
             double c = 54.2;
 
-            x = double(10);
+            int a2 = (int)a;
+            int b2 = (int)b;
+            int c2 = (int)c;
 
+            double x2 = (double)x;
+            double y2 = (double)y;
+            double z2 = (double)z;
+            
+            Console.WriteLine($"Converted to Int: {a2}, {b2}, {c2}");
+            Console.WriteLine($"Converted to Double: {x2}, {y2}, {z2}");
+        }
+
+        static void Strings()
+        {
+            // Console.WriteLine("\\testing\t\'escape\'\nsequences");
+            // Console.WriteLine(@"c:\testing\string\literal:");
+            
+            string message1 = "Testing";
+            string message2 = "Concatation";
+            string message3 = message1 + ' '+ message2;
+
+            // Console.WriteLine(message3);
+            // Console.WriteLine(message3.Length);
+
+            string lengthLessThan3 = "Hello World";
+
+            // for(int i = 0; i < lengthLessThan3.Length; i+=2)
+            // {
+            //     Console.WriteLine(lengthLessThan3[i]);
+            // }
+
+            // Console.WriteLine(message1.ToUpper());
+            // Console.WriteLine(message2.ToLower());
+
+            string newMessage = "";
+
+            for(int i = 1; i < lengthLessThan3.Length - 1; i++)
+            {
+                newMessage += lengthLessThan3[i];
+            }
+            
+            // Console.WriteLine(newMessage);
+
+            string x = "testing";
+            char y = 'e';
+
+            var Letters = new List<char> {'t','e','s','t','i','n','g'};
+
+            for(int i = 0; i < x.Length; i++)
+            {
+                if(Letters[i] == y)
+                {
+                    //Console.WriteLine(i);
+                    break;
+                }
+            }
+
+            if(x.Contains(y))
+            {
+                bool doesContain = true;
+                //Console.WriteLine(doesContain);
+            }
+
+            //Console.WriteLine(String.Join(", ", Letters));
+            // Letters.ForEach(Console.WriteLine);
+
+            // get file name from path 
+
+            string path = "c:\\foo\\bar.txt";
+
+            string[] splitPath = path.Split("\\");
+
+            string fileName = splitPath[splitPath.Length - 1];
+
+            // Console.WriteLine(fileName);
+
+            // get the drive code letter in uppercase
+
+            char drive = path[0];
+
+            // Console.WriteLine(char.ToUpper(drive));
+
+            string messageSplit = "HelloWorld How are you?";
+
+            messageSplit.Split(" ");
+
+            Console.WriteLine(messageSplit);
 
         }
 
@@ -77,7 +164,8 @@ namespace exercises1
         {
             //Variables();
             //MinMax();
-            Operators();
+            //Operators();
+            Strings();
         }
     }
 }
